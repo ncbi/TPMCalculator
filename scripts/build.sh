9ebc7494d54145d05682c935def1d41fd4e97407
Roberto Vera Alvarez <veraalva@ncbi.nlm.nih.gov>
2026-01-28 10:01:01 -0500

    Enhance build script with BAMtools installation
    Added installation steps for BAMtools and updated LDFLAGS.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Installing BAMtools"
 git clone https://github.com/pezmaster31/bamtools
 cd bamtools
 mkdir build
@@ -10,7 +11,18 @@ make install
 cd ..
 export BAMTOOLS_DIR=`pwd`
 export CPPFLAGS="-I $BAMTOOLS_DIR/include/bamtools/"
-export LDFLAGS="-L $BAMTOOLS_DIR/lib64 -Wl,-rpath,$BAMTOOLS_DIR/lib64"
+export LDFLAGS="-L $BAMTOOLS_DIR/lib -Wl,-rpath,$BAMTOOLS_DIR/lib"
+
+echo "Using FLAGS:"
+echo $BAMTOOLS_DIR
+echo $CPPFLAGS
+echo $LDFLAGS
+
+echo "Compiling TPMCalculator"
+pwd
+ls
+echo
+echo
 make
 
 
